893e490df357e21c660017018409a71ddd11622e
Fortune Ekeruo <legalmody@gmail.com>
2020-07-11 04:32:10 +0100

    recieve sent messages on the server

--- ws/chat.ts
+++ ws/chat.ts
@@ -1,15 +1,22 @@
-import { WebSocket } from 'https://deno.land/std/ws/mod.ts';
+import { WebSocket, isWebSocketCloseEvent } from 'https://deno.land/std/ws/mod.ts';
 import { v4 } from 'https://deno.land/std/uuid/mod.ts';
 
 let sockets = new Map<string, WebSocket>();
 
 const chatConn = async (ws: WebSocket) => {
-  console.log('New soocket connection...');
-
   const randomUniqueId = v4.generate();
   sockets.set(randomUniqueId, ws);
 
-  console.log(sockets)
+  for await (const e of ws) {
+
+    if (isWebSocketCloseEvent(e)) {
+      sockets.delete(randomUniqueId);
+    }
+    
+    if (typeof e === 'string') {
+      let event = JSON.parse(e);
+    }
+  }
 }
 
 export { chatConn };
